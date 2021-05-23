d34b89f3efb94aeca16b86c50c88884869a508da
hema omer <32086055+big-yoshi@users.noreply.github.com>
2021-05-23 21:29:39 +0300

    created filters

---
+++ client/src/components/Card.vue
@@ -0,0 +1,93 @@
+<template>
+
+    <div class="content">
+        <div class="bg-img" v-bind:style="{ 'background-image': 'url(' + Link + ')' }">
+            <div class="like-btn-container">
+                <img src="like.svg" alt="">
+            </div>
+            
+        </div>
+        <h1>{{Title}}</h1>
+
+    </div>
+    
+</template>
+
+<style scoped>
+.content{
+    height: 200px;
+    box-shadow: 0px 8px 24px rgba(0, 29, 61, 0.1);
+    border-radius: 10px;
+    width:144px;
+    height: 199px;
+    padding-top:2px;
+    display: grid;
+    place-content: center;
+    position: relative;
+    
+}
+/*
+.content::after{
+    content: "";
+    position: absolute;
+    height: 0%;
+    width: 100%;
+    background:rgba(0, 0,0,0.8);
+    border-radius: 10px;
+    align-self: end;
+    transition: all 0.5s ease-in-out;
+}
+*/
+
+.content:hover::after{
+    height: 100%;
+}
+
+.bg-img{
+    
+    background-position: center;
+    background-size: cover;
+    width: 122px;
+    height: 149px;
+    border-radius:10px;
+    display: grid;
+    position: relative;
+}
+h1{
+    
+    font-family: Poppins;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 12px;
+    text-align: left;
+    color: #001D3D;
+    cursor: pointer;
+}
+
+.like-btn-container{
+    background:white;
+    width: 19px;
+    height: 19px;
+    border-radius: 5px;
+    display: grid;
+    place-items: center;
+    position: absolute;
+    align-self: start;
+    justify-self: end;
+    margin: 6px;
+    cursor: pointer;
+}
+</style>
+
+<script>
+export default {
+    name:"Card",
+    props:{
+        Title: String,
+        Link: String
+    },
+    setup() {
+        
+    },
+}
+</script>
--- client/src/views/Find.vue
+++ client/src/views/Find.vue
@@ -1,18 +1,59 @@
 <template>
-    <div >
+    <div class="search-page">
         <h1>Search</h1>
         <div class="search-section">
             <div class="search-container">
                 <img src="search.svg" alt="" class="search-icon">
             </div>
             <input type="text" placeholder="Find your style">
-            <div class="filter-container">
-                <img src="filter.svg" alt="" class="filter-icon">
+            <div class="filter-btn">
+                <div class="filter-container" >
+                    <img src="filter.svg" alt="" class="filter-icon" v-on:click="showFilter()">
+                </div>
+                <div class="filterMenu">
+
+                    <span id="close" v-on:click="closeFilter()">&#10006;</span>
+                    <div class="lists">
+                        <h3>
+                            Gender
+                        </h3>
+                        <div class="gender">
+                            <span @click="updateFilters('gender','male')" >Male</span>
+                            <span @click="updateFilters('gender','female')">Female</span>
+                            <span @click="updateFilters('gender','all')">All</span>
+                        </div>
+                        <h3>
+                            Type
+                        </h3>
+                        <div class="type">
+                            <span @click="updateFilters('type','top')">Top</span>
+                            <span @click="updateFilters('type','jeans')">Jeans</span>
+                            <span @click="updateFilters('type','shirt')">Shirt</span>
+                            <span @click="updateFilters('type','dress')">Dress</span>
+                            <span @click="updateFilters('type','all')">All</span>
+                        </div>
+                        <h3>
+                            Season
+                        </h3>
+                        <div class="season">
+                            <span @click="updateFilters('season','winter')">Winter</span>
+                            <span @click="updateFilters('season','summer')">Summer</span>
+                            <span @click="updateFilters('season','autumn')">Autumn</span>
+                            <span @click="updateFilters('season','spring')">Spring</span>
+                            <span @click="updateFilters('season','all')">All</span>
+                        </div>
+
+                    </div>
+                    
+
+                </div>
             </div>
+            
+            
 
         </div>
         <div class="content-container">
-
+            <Card v-for="item in result" v-bind:key="item.id" v-bind:Title="item.Title" v-bind:Link="item.img_link"></Card>
         </div>
     </div>    
 </template>
@@ -35,8 +76,72 @@
     place-items: center;
 }
 
+.filters-btn{
+    position: relative;
+    background: #001D3D;
+}
+
+.filterMenu{
+    
+    
+    display: grid;
+    place-items: center;
+    background:white;
+    width: 100%;
+    height: 100%;
+    padding:0;
+    margin: 0;
+    z-index: 99;
+    position: fixed;
+    top: 0;
+    left: 0;
+    
+    display: none;  
+    
+}
+
+.filterMenu .lists {
+    background:grey;
+    width: 90%;
+    display: grid;
+    place-items: center;
+    text-align: left;
+    height:70%;
+    
+}
 
 
+.filterMenu .lists h3{
+    place-self: start;
+    font-size: 24px;
+}
+.filterMenu .lists div{
+    
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    width: 100%;
+    
+    height: 110%;
+}
+.filterMenu .lists span{
+    
+    font-size: 14px;
+    border:1.4px solid rgba(0, 29, 61, 0.45);
+    font-weight:200;
+    padding: 10px 24px 10px 24px;
+    border-radius: 25px;
+    transition: all 0.5s ease-in-out;
+}
+.filterMenu .lists span:hover{
+    background: #001D3D;
+    color: white;
+    cursor: pointer;
+}
+.filterMenu .lists span.active{
+    background: #001D3D;
+    color: white;
+    cursor: pointer;
+}
 .search-section img{
 
 }
@@ -81,17 +186,76 @@
 }
 
 .content-container{
-    background: green;
+    
     height: auto;
     display: grid;
     width: 100%;
+    grid-template-columns: auto auto;
+    place-content: start;
+    
+    margin-top: 50px;
+    justify-content: space-between;
+    align-content: space-between;
+    grid-gap: 36px;
+
+}
+#close{
+    position: absolute;
+    align-self: start;
+    justify-self: start;
+    top: 20px;
+    left: 18px;
+    font-size: 25px;
+    cursor: pointer;
 }
 </style>
 
 <script>
+import Card from '@/components/Card.vue'
+import { gsap } from 'gsap';
+import { CSSPlugin } from 'gsap/CSSPlugin'
+gsap.registerPlugin(CSSPlugin);
 export default {
-    setup() {
-        
+    components:{
+        Card
     },
+    data: function(){
+        return {
+            result: [
+                {Title:"Grey shirt", gender:"male", type:"top" ,img_link:'https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg?auto=compress&cs=tinysrgb&h=650&w=940'},
+                {Title: "leather jacket",gender:"female", type:'top',img_link:"https://images.pexels.com/photos/1034859/pexels-photo-1034859.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"}
+            ],
+            
+            filters:{
+                gender:"all",
+                type: "all",
+                season: "all"
+            }
+        }
+    },
+    methods:{
+            showFilter: function(){
+                gsap.to(".filterMenu", {opacity:1,zIndex:99,duration:0.4});
+                gsap.to(".filterMenu",{display:'grid'});
+            },
+            closeFilter: function(){
+                gsap.to(".filterMenu", {opacity:0,zIndex:-1, duration:0.3});
+                gsap.to(".filterMenu",{display:'none'});
+                
+            },
+            
+            updateFilters: function(cate, val){
+                this.filters[cate] = val;
+            }
+        },
+    watch:{
+            filters:{
+                deep:true,
+                handler(val){
+                    console.log(val);
+                }
+            }
+        }
+
 }
 </script>
